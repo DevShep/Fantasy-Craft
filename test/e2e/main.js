fc4383628a6d0e6135e780f79ecbf7c6b1f76ef9
Jacob H <jacob.k.haines@gmail.com>
2014-09-17 03:40:18 -0400

    adds single test for accessing attributes

--- test/e2e/main.js
+++ test/e2e/main.js
@@ -1,9 +1,25 @@
 'use strict';
 
 // main.js
-describe('angularjs homepage', function() {
+describe('app homepage', function() {
   it('should have a title', function() {
     browser.get('index.html');
     expect(browser.getTitle()).toEqual('Fantasy Craft Character Sheet');
+  });
+
+  it('should set attribute scores', function() {
+  	/**
+  	 * @todo write functions for the below locators to attributes can be easily found
+  	 */
+  	var strRow = element(by.repeater('attr in Char.attributes').row(0));
+  	var strScore = strRow.element(by.model('attr.score'));
+  	var strScoreMod = strRow.element(by.binding('attr.getScoreMod()'));
+  	var strImpaired = strRow.element(by.model('attr.impaired'));
+  	var strImpairedMod = strRow.element(by.binding('attr.getImpairedMod()'));
+
+  	strScore.sendKeys('14');
+  	strImpaired.sendKeys('10');
+  	expect(strScoreMod.getText()).toBe('2');
+  	expect(strImpairedMod.getText()).toBe('0');
   });
 });
